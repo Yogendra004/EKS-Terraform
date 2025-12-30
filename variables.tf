a4ab07d452d862a8eefcbff14a70d56ba0263b3f
Yogendra Singh <100386465+Yogendra004@users.noreply.github.com>
2025-12-30 15:14:23 +0000

    Change default SSH key name in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "mywebservey-key.pem"
+  default     = "mywebservey-key"
 }
-
